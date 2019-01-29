08423a8195e15ed4661693abb570460f033e65e0
MattiasTilertThunqvist <mattias@tilert.com>
2019-01-29 17:16:31 +0100

    - Setting up the project

---
+++ Pizza_Ordering_Web_App/Pizza_Ordering_Web_App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pizza_Ordering_Web_App
+//
+//  Created by Mattias Tilert Thunqvist on 2019-01-29.
+//  Copyright © 2019 Mattias Tilert Thunqvist. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
